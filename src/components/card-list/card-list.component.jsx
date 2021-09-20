d32e57affe9ee5c35fb3a9201b95ac452afdc869
Edwin Castro <59403733+Edwin-Castro@users.noreply.github.com>
2021-09-19 21:59:40 -0400

    added card folder & file & robohash props to file

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import { Card } from '../card/card.component'
 import './card-list.styles.css'
 
 export const CardList = (props) => {
   return (<div className='card-list'>
     {props.monsters.map(monsters => (
-      <h1 key={monsters.id}> {monsters.name} </h1>
+      <Card key={monsters.id} monsters={monsters} />
     ))}
   </div>)
 }
